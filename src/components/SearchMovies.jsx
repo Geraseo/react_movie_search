d4aef216dd800fdf3e3e7531fbffbf142834ce37
Geraseo <tomt78872@gmail.com>
2023-03-30 10:53:22 +0200

    final updates

--- src/components/SearchMovies.jsx
+++ src/components/SearchMovies.jsx
@@ -1,12 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { searchMovie } from "../services/api";
 import "./SearchMovies.css";
 
 const SearchMovies = ({ setResults }) => {
   const [searchWord, setSearchWord] = useState("");
+  const inputRef = useRef("");
+
   // 1. uzsettinti paieskos zodi i state
-  const handleChange = (searchWord) => {
-    setSearchWord(searchWord);
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchWord(inputRef.current.value);
+    getMovies(searchWord);
   };
   // 2. funkcija kreppiasi i services api
   const getMovies = (searchWord) => {
@@ -24,12 +28,27 @@ const SearchMovies = ({ setResults }) => {
       <input
         className="searchInput"
         type="text"
-        onChange={(e) => handleChange(e.target.value)}
-        value={searchWord}
-        placeholder="Search for movies..."
+        ref={inputRef}
+        placeholder="Search for movie"
       />
+      <button className="btn" onClick={handleChange}>
+        Search
+      </button>
     </div>
   );
 };
 
 export default SearchMovies;
+
+{
+  /* <div>
+      <input
+        className="searchInput"
+        type="text"
+        onChange={(e) => handleChange(e.target.value)}
+        value={searchWord}
+        placeholder="Search for movies..."
+      />
+      <button className="btn">Search</button>
+    </div> */
+}
